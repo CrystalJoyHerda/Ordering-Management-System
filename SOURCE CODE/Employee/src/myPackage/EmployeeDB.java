debe3151e15c1a001189597f2849fdfae341b127
CrystalHerda <crystaljoyherda@gmail.com>
2025-03-04 18:35:34 +0800

    commit files for log in adn dashboard

--- SOURCE CODE/Employee/src/myPackage/EmployeeDB.java
+++ SOURCE CODE/Employee/src/myPackage/EmployeeDB.java
@@ -8,7 +8,7 @@ public class EmployeeDB {
 
     private static final String URL = "jdbc:mysql://localhost:3306/employee_db";
     private static final String USER = "emp";
-    private static final String PASSWORD = "emp"
+    private static final String PASSWORD = "emp";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
